cc9ae9e8a0143b951c75eb1daa2b2fbd02a6e37e
Dacamo <castrom1998@gmail.com>
2020-04-13 22:56:19 -0500

    Improved createAsset form in modal
    Co-Authored-By: Edwin Osorio <edwin1106@users.noreply.github.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

--- src/components/ModalCreateAsset.js
+++ src/components/ModalCreateAsset.js
@@ -2,42 +2,45 @@ import React from 'react';
 import Modal from './Modal';
 
 const getHeader = () => {
-    return (
-        <h5 className="modal-title" id="exampleModalLabel">New Asset</h5>
-    );
+  return (
+    <h5 className="modal-title" id="exampleModalLabel">New Asset</h5>
+  );
 }
 
 const getFooter = () => {
-    return(
-        <>
-          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        </>
-    );
+  return (
+    <>
+      <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+    </>
+  );
 }
 
-const ModalCreateAsset =({id}) => {
-    return (
-        <Modal id= {id} header={getHeader()} footer={getFooter()}>
-            <div>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="exampleInputEmail1">Email address</label>
-                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="exampleInputPassword1">Password</label>
-                  <input type="password" className="form-control" id="exampleInputPassword1"/>
-                </div>
-                <div className="form-group form-check">
-                  <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                  <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-              </form>
-            </div>
-        </Modal>
-    )
+const ModalCreateAsset = ({ id }) => {
+  return (
+    <Modal id={id} header={getHeader()} footer={getFooter()}>
+      <div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input className="form-control" id="title"  />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <input className="form-control" id="description" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="tag">Tags</label>
+            <input className="form-control" id="tag" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="imageURL">Image URL</label>
+            <input className="form-control" id="imageURL" />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">Create</button>
+        </form>
+      </div>
+    </Modal>
+  )
 }
 
 export default ModalCreateAsset;
